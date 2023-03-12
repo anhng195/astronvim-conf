54e849b8aa57eafd98e34a582174822f6caedd1d
Anh Nguyen <anhngw@gmail.com>
2023-03-12 10:24:45 +0700

    update options.lua and plugins/user.lua

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,5 +9,35 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
-  { "ellisonleao/glow.nvim", config = true, cmd = "Glow" }
+  { "ellisonleao/glow.nvim", config = true, cmd = "Glow" },
+  {
+    "iamcco/markdown-preview.nvim",
+    ft = "markdown",
+    build = function()
+      vim.fn["mkdp#util#install"]()
+    end,
+  },
+  {
+    'nvim-neorg/neorg',
+    ft = 'norg',   -- lazy load on filetype
+    cmd = 'Neorg', -- lazy load on command, allows you to autocomplete :Neorg regardless of whether it's loaded yet
+    --  (you could also just remove both lazy loading things)
+    priority = 30, -- treesitter is on default priority of 50, neorg should load after it.
+    config = function()
+      require('neorg').setup {
+        load = {
+          ["core.defaults"] = {},       -- Loads default behaviour
+          ["core.norg.concealer"] = {}, -- Adds pretty icons to your documents
+          ["core.norg.completion"] = {},
+          ["core.norg.dirman"] = {      -- Manages Neorg workspaces
+            config = {
+              workspaces = {
+                notes = "~/notes",
+              },
+            },
+          },
+        },
+      }
+    end
+  },
 }
