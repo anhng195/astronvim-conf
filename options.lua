7c3f0195e86da7495481243f4e62eea97d7c1f53
Anh Nguyen <anhngw@gmail.com>
2023-05-15 10:33:51 +0700

    add mouse config

--- options.lua
+++ options.lua
@@ -8,6 +8,7 @@ return {
     signcolumn = "auto",   -- sets vim.opt.signcolumn to auto
     wrap = false,          -- sets vim.opt.wrap
     clipboard = "",
+    mouse = "nvc",
   },
   g = {
     mapleader = " ",                 -- sets vim.g.mapleader
